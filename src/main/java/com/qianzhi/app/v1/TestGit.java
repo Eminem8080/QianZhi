18c9b9519c05948ed77f76eb90dd261393d3ee61
Eminem8080 <hx807163466@gmail.com>
2015-04-22 14:48:06 +0800

    App

---
+++ src/main/java/com/qianzhi/app/v1/TestGit.java
@@ -0,0 +1,17 @@
+package com.qianzhi.app.v1;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by admin on 2015/4/22.
+ */
+public class TestGit extends Fragment{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
